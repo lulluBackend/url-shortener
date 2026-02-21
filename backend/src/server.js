567f73f22edccf6229c574eed4e853d9b2649f32
lullu <llucas428x@gmail.com>
2026-02-21 09:20:54 -0300

    1.3

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,30 @@
-require("dotenv").config();
+require('dotenv').config();
+
+const linkRoutes = require("./routes/linkRoutes");
 
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
-
-const linkRoutes = require("./routes/linkRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", linkRoutes);
 
-// SERVIR FRONTEND
-app.use(express.static(path.join(__dirname, "../../frontend")));
+mongoose.connect("mongodb+srv://lulluPk_db_user:LDA7sTrAGxnaP5MK@cluster0.2uqh5ft.mongodb.net/urlshortener?retryWrites=true&w=majority")
+  .then(() => console.log("Mongo conectado"))
+  .catch(err => console.log(err));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+app.get("/", (req, res) => {
+  res.send("API rodando");
+});
+
+app.listen(5000, () => {
+  console.log("Servidor rodando na porta 5000");
 });
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Mongo conectado"))
   .catch(err => console.error("Erro ao conectar:", err));
-
-app.listen(5000, () => {
-  console.log("Servidor rodando na porta 5000");
-});
